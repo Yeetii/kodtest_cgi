10a1dc4bb37a07ab3ee2a4ae8ce13cd88abb6e65
Erik Magnusson <magnusserik@gmail.com>
2022-03-04 11:01:25 -0800

    Passing tests

--- frequency-api.spec.ts
+++ frequency-api.spec.ts
@@ -1,13 +1,21 @@
 const fun = require("./frequency-api");
 
+test("Clean input", () => {
+  expect(fun.cleanInput("hUnd kaTT")).toStrictEqual(["Hund", "Katt"]);
+});
+
+test("Count word occurance", () => {
+  expect(fun.countFrequencies("Hund Hund")).toStrictEqual({ Hund: 2 });
+});
+
 test("Case insensitive", () => {
-  expect(fun.countFrequencies("Hund huND")).toBe({ Hund: 2 });
+  expect(fun.countFrequencies("Hund huND")).toStrictEqual({ Hund: 2 });
 });
 
 test("Sorted after frequency", () => {
   expect(
     fun.countFrequencies("Banan Äpple Katt hund Banan Hund katt Hund")
-  ).toBe({
+  ).toStrictEqual({
     Hund: 3,
     Banan: 2,
     Katt: 2,
@@ -15,7 +23,25 @@ test("Sorted after frequency", () => {
   });
 });
 
-test("Only return 10 most frequent", () => {
+test("Only return 10 keys", () => {
   const manyWordCall = fun.countFrequencies("a b c d e f g h i j k l m n o p");
-  expect(Object.keys(manyWordCall).length);
+  expect(Object.keys(manyWordCall).length).toBe(10);
+});
+
+test("Only return 10 most frequent", () => {
+  const manyWordCall = fun.countFrequencies(
+    "a a b b c c d d e e f f g g h h i i j k l m n o p p"
+  );
+  expect(manyWordCall).toStrictEqual({
+    A: 2,
+    B: 2,
+    C: 2,
+    D: 2,
+    E: 2,
+    F: 2,
+    G: 2,
+    H: 2,
+    I: 2,
+    P: 2,
+  });
 });
--- frequency-api.ts
+++ frequency-api.ts
@@ -4,8 +4,42 @@ const app = express();
 const hostname = "127.0.0.1";
 const port = 3000;
 
-const countFrequencies = (words: string) => {
-  return { Hund: 2 };
+const countFrequencies = (wordsString: string) => {
+  const words = cleanInput(wordsString);
+
+  let freqDict = {};
+  words.forEach((word) => {
+    if (!freqDict[word]) {
+      freqDict[word] = 1;
+    } else {
+      freqDict[word]++;
+    }
+  });
+  freqDict = top10Dict(freqDict);
+  return freqDict;
+};
+
+const top10Dict = (freqDict) => {
+  var items = Object.keys(freqDict).map(function (key: string) {
+    return [key, freqDict[key]];
+  });
+  items.sort(function (first, second) {
+    return second[1] - first[1];
+  });
+  items = items.slice(0, 10);
+
+  const shortDict = {};
+  items.forEach((item) => {
+    shortDict[item[0]] = item[1];
+  });
+  return shortDict;
+};
+
+const cleanInput = (wordsString: string) => {
+  const words = wordsString.split(" ");
+  return words.map((word) => {
+    return word.charAt(0).toUpperCase() + word.slice(1).toLowerCase();
+  });
 };
 
 app.get("/", function (_req, res) {
@@ -17,3 +51,4 @@ var server = app.listen(port, hostname, function () {
 });
 
 exports.countFrequencies = countFrequencies;
+exports.cleanInput = cleanInput;
